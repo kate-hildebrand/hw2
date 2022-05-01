77c5eb632a01f55b9f4304f8aaad402570ca66b2
kate-hildebrand <kate.hildebrand@kellogg.northwestern.edu>
2022-05-01 23:16:41 +0000

    final

--- kmdb.rb
+++ kmdb.rb
@@ -1,22 +1,3 @@
-# In this assignment, you'll be using the domain model from hw1 (found in the hw1-solution.sql file)
-# to create the database structure for "KMDB" (the Kellogg Movie Database).
-# The end product will be a report that prints the movies and the top-billed
-# cast for each movie in the database.
-
-# To run this file, run the following command at your terminal prompt:
-# `rails runner kmdb.rb`
-
-# Requirements/assumptions
-#
-# - There will only be three movies in the database – the three films
-#   that make up Christopher Nolan's Batman trilogy.
-# - Movie data includes the movie title, year released, MPAA rating,
-#   and studio.
-# - There are many studios, and each studio produces many movies, but
-#   a movie belongs to a single studio.
-# - An actor can be in multiple movies.
-# - Everything you need to do in this assignment is marked with TODO!
-
 # Rubric
 # 
 # There are three deliverables for this assignment, all delivered within
@@ -30,52 +11,19 @@
 # - Query the data and loop through the results to display output similar to the
 #   sample "report" below. (10 points)
 
-# Submission
-# 
-# - "Use this template" to create a brand-new "hw2" repository in your
-#   personal GitHub account, e.g. https://github.com/<USERNAME>/hw2
-# - Do the assignment, committing and syncing often
-# - When done, commit and sync a final time before submitting the GitHub
-#   URL for the finished "hw2" repository as the "Website URL" for the 
-#   Homework 2 assignment in Canvas
-
-# Successful sample output is as shown:
-
-# Movies
-# ======
-
-# Batman Begins          2005           PG-13  Warner Bros.
-# The Dark Knight        2008           PG-13  Warner Bros.
-# The Dark Knight Rises  2012           PG-13  Warner Bros.
-
-# Top Cast
-# ========
-
-# Batman Begins          Christian Bale        Bruce Wayne
-# Batman Begins          Michael Caine         Alfred
-# Batman Begins          Liam Neeson           Ra's Al Ghul
-# Batman Begins          Katie Holmes          Rachel Dawes
-# Batman Begins          Gary Oldman           Commissioner Gordon
-# The Dark Knight        Christian Bale        Bruce Wayne
-# The Dark Knight        Heath Ledger          Joker
-# The Dark Knight        Aaron Eckhart         Harvey Dent
-# The Dark Knight        Michael Caine         Alfred
-# The Dark Knight        Maggie Gyllenhaal     Rachel Dawes
-# The Dark Knight Rises  Christian Bale        Bruce Wayne
-# The Dark Knight Rises  Gary Oldman           Commissioner Gordon
-# The Dark Knight Rises  Tom Hardy             Bane
-# The Dark Knight Rises  Joseph Gordon-Levitt  John Blake
-# The Dark Knight Rises  Anne Hathaway         Selina Kyle
 
 # Delete existing data, so you'll start fresh each time this script is run.
+# TODO!
 Movie.destroy_all
 Studio.destroy_all
 Actor.destroy_all
 Role.destroy_all
-# TODO!
 
 # Generate models and tables, according to the domain model.
 # TODO!
+# done using rails generate model ____ in the terminal, then going into the db/migrate folder to add the column headers.
+# please check those 4 migrations for this step 
+
 
 # Insert data into the database that reflects the sample data shown above.
 # Do not use hard-coded foreign key IDs.
@@ -265,6 +213,7 @@ puts "======"
 
 # Query the movies data and loop through the results to display the movies output.
 # TODO!
+# Note: I do not know how to format this into a table 
 
 all_movies = Movie.all
 for movie in all_movies
